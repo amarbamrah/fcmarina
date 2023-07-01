f26837efd1d2e6ad5a19c4173ccd1ad3218e7e43
amarbamrah <jontybamrah@hotmail.com>
2023-07-01 16:17:42 +0530

    ds

--- resources/views/tc.blade.php
+++ resources/views/tc.blade.php
@@ -6,7 +6,11 @@
     <title>TC</title>
 </head>
 <body>
-    <h1>Terms & Conditions</h1>
     <h4>Your rights and responsibilities when using our services</h4>
+
+<p>FC MARINA BOOKINGS APP enables people to connect with each other, build connections, host, find locations that cater to their needs and grow their businesses.
+By accessing and using www.FC MARINA BOOKINGS APP.in (Website) and/or FC MARINA BOOKINGS APP application (App), the user accepts and agrees to be bound by the Terms of Service. Please read these Terms carefully. Your use of the Website and the App (the Website and the App are collectively referred to as the ‘Platform’) is at your own risk, including the risk that you might be exposed to content that is objectionable or otherwise inappropriate.
+The Platform is owned and operated by M/s. FC MARINA BOOKINGS APP SPORTS INFRA Private Limited, a private limited company incorporated under the Companies Act, 2013 and having its registered at having its Registered Office at (address)
+</p>
 </body>
 </html>
