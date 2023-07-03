b1685e9829b2a65d25572317960d80ab84223951
amarbamrah <jontybamrah@hotmail.com>
2023-07-03 13:49:04 +0530

    ds

--- resources/views/tc.blade.php
+++ resources/views/tc.blade.php
@@ -12,5 +12,19 @@
 By accessing and using www.FC MARINA BOOKINGS APP.in (Website) and/or FC MARINA BOOKINGS APP application (App), the user accepts and agrees to be bound by the Terms of Service. Please read these Terms carefully. Your use of the Website and the App (the Website and the App are collectively referred to as the ‘Platform’) is at your own risk, including the risk that you might be exposed to content that is objectionable or otherwise inappropriate.
 The Platform is owned and operated by M/s. FC MARINA BOOKINGS APP SPORTS INFRA Private Limited, a private limited company incorporated under the Companies Act, 2013 and having its registered at having its Registered Office at (address)
 </p>
+
+<h3>Defination</h3>
+<ul>
+    <li>The terms ‘user’ or ‘you’ or ‘your’ refer to you, as a user of the Platform. A user is a person who accesses or uses the Platform for the purposes of sharing, displaying, hosting, publishing, transacting, or uploading information or views or pictures and includes other persons jointly participating in using the Platform.
+        
+</li>
+
+<li>The terms "FC MARINA BOOKINGS APP', "we", "us" or "our" shall mean M/s. FC MARINA BOOKINGS APP SPORTS INFRA Private Limited.
+</li>
+
+<li>
+The Terms, together with the ‘Privacy Policy’, shall constitute the entire agreement between you and us concerning the Platform. No failure or delay by us in exercising any right, power or privilege under the Terms shall operate as a waiver of such right or acceptance of any variation of the Terms and nor shall any single or partial exercise by either party of any right, power or privilege preclude any further exercise of that right or the exercise of any other right, power or privilege.
+</li>
+</ul>
 </body>
 </html>
