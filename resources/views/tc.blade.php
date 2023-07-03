30f51e0ec142d091032730a20ea70dbd95203c69
amarbamrah <jontybamrah@hotmail.com>
2023-07-03 13:50:42 +0530

    ds

--- resources/views/tc.blade.php
+++ resources/views/tc.blade.php
@@ -26,5 +26,34 @@ The Platform is owned and operated by M/s. FC MARINA BOOKINGS APP SPORTS INFRA P
 The Terms, together with the ‘Privacy Policy’, shall constitute the entire agreement between you and us concerning the Platform. No failure or delay by us in exercising any right, power or privilege under the Terms shall operate as a waiver of such right or acceptance of any variation of the Terms and nor shall any single or partial exercise by either party of any right, power or privilege preclude any further exercise of that right or the exercise of any other right, power or privilege.
 </li>
 </ul>
+
+
+<h3>Services offered by FC MARINA BOOKINGS APP
+</h3>
+
+<ol>
+    <li>FC MARINA BOOKINGS APP provides its users a personalised experience using the data provided to us by you to show posts, events, stadiums, activity centres and other features you might use.
+</li>
+
+<li>
+FC MARINA BOOKINGS APP enables you to connect with people, group, businesses, organisations, and others. We use the data provided to us by you to make suggestions for you and to personalise your experience. FC MARINA BOOKINGS APP enables you to explore events and tournaments that are conducted near your locality and connect with sportsmen who share similar interest as you.
+
+</li>
+
+<li>
+FC MARINA BOOKINGS APP enables you to book stadiums or activity centres (Venues) at the various locations listed and offered for rent on the Platform. Users can also create events or tournaments to connect with other users on the Platform. For booking stadiums or activity centres, we may charge the users of the Platform a service fee (inclusive of applicable taxes whenever not expressly mentioned) determined on the basis of various factors including but not limited to duration of the rental, demand for the stadium or activity centre, weather conditions, seasonal peaks or other such parameters as may be determined from time to time.
+
+</li>
+
+<li>
+FC MARINA BOOKINGS APP enables you to communicate with your connections by enabling you to post photos, videos and stories across the FC MARINA BOOKINGS APP platform, send messages directly to your connections or people who are using the Platform, view photos, videos and stories posted on the Platform by other users, and by helping you to find people, groups, businesses, organisations and others.
+
+</li>
+
+<li>
+We display ads, offers and other sponsored content to help you discover content,products, and services that are offered by our turfs. 
+
+</li>
+</ol>
 </body>
 </html>
