3649d2a9e060a73ddf3c20cbe348c0fc4bd53e90
amarbamrah <jontybamrah@hotmail.com>
2023-07-03 09:40:06 +0530

    ds

--- resources/views/admin/masters/locations/index.blade.php
+++ resources/views/admin/masters/locations/index.blade.php
@@ -3,38 +3,60 @@
 
 @section('content')
 <div class="page-content">
-<nav class="page-breadcrumb">
-	<ol class="breadcrumb">
-    	<li class="breadcrumb-item"><a href="#">Masters</a></li>
-    	<li class="breadcrumb-item"><a href="#">Locations</a></li>
+    <nav class="page-breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Masters</a></li>
+            <li class="breadcrumb-item"><a href="#">Locations</a></li>
 
-	</ol>
-</nav>
+        </ol>
+    </nav>
 
-<div class="row">
-	<div class="col-md-12 grid-margin stretch-card">
-        <div class="card">
-            <div class="card-body">
-             <div class="table-responsive">
-               <table id="dataTableExample" class="table">
-                    <thead>
-                      <tr>
-                        <th>#</th>
-                      <th>Title</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($locations as $i=>$loc)
-                      <tr>
-                      <td>{{$i+1}}</td>
-                      <td>{{$loc->name}}</td>
-                      </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
+    <div class="row">
+
+    <div class="col-md-6 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <form action="/admin/locations" method="post">
+                        @csrf 
+                       <div class="form-group">
+                        <label for="">
+                            Title
+                        </label>
+
+                        <input type="text" name="title" class="form-control">
+                       </div>
+
+                       <button type="submit" class="btn btn-primary">ADD LOCATION</button>
+                    </form>
                 </div>
-              </div>
+                </div>
+                </div>
+
+
+
+        <div class="col-md-6 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table id="dataTableExample" class="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($locations as $i=>$loc)
+                                <tr>
+                                    <td>{{$i+1}}</td>
+                                    <td>{{$loc->name}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-	</div>
-</div>
-@endsection
+        </div>
+    </div>
+    @endsection
