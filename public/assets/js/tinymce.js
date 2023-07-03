e6aff751c4f6fec0462747df39da2b32099e9251
amarbamrah <jontybamrah@hotmail.com>
2023-07-03 09:51:46 +0530

    ds

--- public/assets/js/tinymce.js
+++ public/assets/js/tinymce.js
@@ -10,12 +10,13 @@ $(function() {
       selector: '#tinymceExample',
       min_height: 350,
       default_text_color: 'red',
+      menu:false,
+      promotion:false,
       plugins: [
         'advlist', 'autoresize', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'pagebreak',
         'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen',
       ],
-      toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-      toolbar2: 'print preview media | forecolor backcolor emoticons | codesample help',
+      toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
       image_advtab: true,
       templates: [{
           title: 'Test template 1',
