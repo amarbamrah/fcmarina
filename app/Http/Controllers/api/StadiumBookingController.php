f32159393dcdb1d1aed8ed0eda47111e8556290f
amarbamrah <jontybamrah@hotmail.com>
2023-07-08 12:37:36 +0530

    ds

--- app/Http/Controllers/api/StadiumBookingController.php
+++ app/Http/Controllers/api/StadiumBookingController.php
@@ -125,7 +125,7 @@ class StadiumBookingController extends Controller
 
 
 
-            return $url;
+            return ['url'=>$url];
             
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
