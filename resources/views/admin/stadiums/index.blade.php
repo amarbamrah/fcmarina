a66401dacaa883790e8a0f55b7aa77a5c1eef922
amarbamrah <jontybamrah@hotmail.com>
2023-07-14 14:05:40 +0530

    ds

--- resources/views/admin/stadiums/index.blade.php
+++ resources/views/admin/stadiums/index.blade.php
@@ -14,7 +14,7 @@
         <div class="card">
             <div class="card-body">
              <div class="">
-               <table class="table">
+               <table class="table responsive">
                     <thead>
                       <tr>
                       <th> Stadium Image</th>
