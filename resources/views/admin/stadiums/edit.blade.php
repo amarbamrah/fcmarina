00e617ab232a762c3d2e0eaeaf56f50708facac7
amarbamrah <jontybamrah@hotmail.com>
2023-07-14 13:41:33 +0530

    ds

--- resources/views/admin/stadiums/edit.blade.php
+++ resources/views/admin/stadiums/edit.blade.php
@@ -83,7 +83,7 @@
                             <div class="col-sm-6">
                                 <div class="mb-3">
                                     <label class="form-label">Featured Image</label>
-                                    <input type="file" id="myDropify" data-default-file="{{url($st->images[0]->image)}}"  class="form-control" name="image">
+                                    <input type="file" id="myDropify" data-default-file="{{url(count($st->images)==0?'': $st->images[0]->image)}}"  class="form-control" name="image">
                                 </div>
                             </div><!-- Col -->
                         </div>
