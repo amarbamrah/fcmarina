0dd6fad8d3206ce02c409da3e74f77b036e28c41
amarbamrah <jontybamrah@hotmail.com>
2023-09-21 16:13:25 +0530

    fd

--- app/Http/Controllers/api/StadiumController.php
+++ app/Http/Controllers/api/StadiumController.php
@@ -170,7 +170,7 @@ class StadiumController extends Controller
 
             $username = $user == null ? $sb->name : $user->name;
 
-            $color='#60e3f7';
+            $color='#60e3f730';
 
 
             if($sb->stadium_type=='5s'){
