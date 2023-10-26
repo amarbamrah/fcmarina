40786be39368dc6a0a25b01cbff49cd5db58e960
amarbamrah <jontybamrah@hotmail.com>
2023-10-26 21:21:38 +0530

    ds

--- resources/views/admin/stadiumbookings/index.blade.php
+++ resources/views/admin/stadiumbookings/index.blade.php
@@ -3,122 +3,128 @@
 @section('content')
 <div class="page-content">
 
-             <div class="card">
-             <div class="card-header pb-0">
-                    <div class="d-flex justify-content-between">
-                        <h4 class="card-title mg-b-2 mt-2">Filters</h4>
-                        <i class="mdi mdi-dots-horizontal text-gray"></i>
-                    </div>
-
-                </div>
-                <div class="card-body">
-                    <form method="get" action="/admin/stadium-bookings">
-                        <div class="row">
-
-                        <div class="form-group  col-md-4 mb-3">
-                            <label class="form-label">Stadium:</label>
-                            <select name="stadium" class="form-select" id="">
-                              <option value="all">All</option>
-                              @foreach($stds as $st)
-                                 <option value="{{$st->id}}" {{Request::get('stadium')==$st->id?'selected':''}}>{{$st->name}}</option>
-                              @endforeach
-
-                            </select>
-                        </div>
-                        <div class="form-group  col-md-4 mb-3">
-                            <label class="form-label">Date:</label>
-                            <input type="date" class="form-control" value="{{Request::get('date')==null? \Carbon\Carbon::now()->format('Y-m-d') : Request::get('date')}}" name="date">
-                        </div>
-
-
-                         <div class="form-group  col-md-4 mb-3">
-                            <label class="form-label">Status:</label>
-                            <select name="status" class="form-select" id="">
-                              <option value="all" {{Request::get('status')=='all'?'selected':''}}>All</option>
-                              <option value="Confirmed" {{Request::get('status')=='Confirmed'?'selected':''}}>Confirmed</option>
-                              <option value="Cancelled" {{Request::get('status')=='Cancelled'?'selected':''}}>Cancelled</option>
-
-                            </select>
-                        </div>
-
-                      
-                    
-                    </div>
-                    <input type="submit" class="btn btn-primary" value="Filter Data" />
-                      </form>
-                    </div>
-               </div>
-
-
-
-
-        <div class="card mb-3 mt-3">
+    <div class="card">
         <div class="card-header pb-0">
-                    <div class="d-flex justify-content-between">
-                        <h4 class="card-title mg-b-2 mt-2">All Stadium Bookings</h4>
-                        <i class="mdi mdi-dots-horizontal text-gray"></i>
+            <div class="d-flex justify-content-between">
+                <h4 class="card-title mg-b-2 mt-2">Filters</h4>
+                <i class="mdi mdi-dots-horizontal text-gray"></i>
+            </div>
+
+        </div>
+        <div class="card-body">
+            <form method="get" action="/admin/stadium-bookings">
+                <div class="row">
+
+                    <div class="form-group  col-md-4 mb-3">
+                        <label class="form-label">Stadium:</label>
+                        <select name="stadium" class="form-select" id="">
+                            <option value="all">All</option>
+                            @foreach($stds as $st)
+                            <option value="{{$st->id}}" {{Request::get('stadium')==$st->id?'selected':''}}>{{$st->name}}
+                            </option>
+                            @endforeach
+
+                        </select>
+                    </div>
+                    <div class="form-group  col-md-4 mb-3">
+                        <label class="form-label">Date:</label>
+                        <input type="date" class="form-control"
+                            value="{{Request::get('date')==null? \Carbon\Carbon::now()->format('Y-m-d') : Request::get('date')}}"
+                            name="date">
                     </div>
 
-                </div>
-            <div class="card-body">
-             <div class="table-responsive">
-               <table id="bookingTable" class="table">
-                    <thead>
-                      <tr>
-                      <th>Booking ID</th>
 
-                        <th>User</th>
-                        <th>User Phone</th>
-                        <th>Stadium Name</th>
-                       <th>Stadium Type</th>
-                       <th>Booking Date</th>
-                       <th>Booking Time</th>
-                       <th>Total Amount</th>
-                       <th>Discount</th>
-                       <th>Advance</th>
-                       <th>Rem Amount</th>
-                       <th>Status</th>
-                       <th>Action</th>
-                      </tr>
+                    <div class="form-group  col-md-4 mb-3">
+                        <label class="form-label">Status:</label>
+                        <select name="status" class="form-select" id="">
+                            <option value="all" {{Request::get('status')=='all'?'selected':''}}>All</option>
+                            <option value="Confirmed" {{Request::get('status')=='Confirmed'?'selected':''}}>Confirmed
+                            </option>
+                            <option value="Cancelled" {{Request::get('status')=='Cancelled'?'selected':''}}>Cancelled
+                            </option>
+
+                        </select>
+                    </div>
+
+
+
+                </div>
+                <input type="submit" class="btn btn-primary" value="Filter Data" />
+            </form>
+        </div>
+    </div>
+
+
+
+
+    <div class="card mb-3 mt-3">
+        <div class="card-header pb-0">
+            <div class="d-flex justify-content-between">
+                <h4 class="card-title mg-b-2 mt-2">All Stadium Bookings</h4>
+                <i class="mdi mdi-dots-horizontal text-gray"></i>
+            </div>
+
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="bookingTable" class="table">
+                    <thead>
+                        <tr>
+                            <th>Booking ID</th>
+
+                            <th>User</th>
+                            <th>User Phone</th>
+                            <th>Stadium Name</th>
+                            <th>Stadium Type</th>
+                            <th>Booking Date</th>
+                            <th>Booking Time</th>
+                            <th>Total Amount</th>
+                            <th>Discount</th>
+                            <th>Advance</th>
+                            <th>Rem Amount</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @foreach($stadiumbooking as $i=>$stdbook)
-                                <tr>
-                                  <td>{{$stdbook->booking_id}}</td>
-                                <td>{{$stdbook['user']==null?$stdbook->name:$stdbook['user']['name']}}</td> 
-                                <td>{{$stdbook['user']==null?$stdbook->Phone:$stdbook['user']['phonenumber']}}</td> 
+                        @foreach($stadiumbooking as $i=>$stdbook)
+                        <tr>
+                            <td>{{$stdbook->booking_id}}</td>
+                            <td>{{$stdbook['user']==null?$stdbook->name:$stdbook['user']['name']}}</td>
+                            <td>{{$stdbook['user']==null?$stdbook->Phone:$stdbook['user']['phonenumber']}}</td>
 
-                                <td>{{$stdbook['stadium']['name']}}</td> 
-                                    <td>{{$stdbook->stadium_type}}</td>
-                                    <td>{{$stdbook->date}}</td>
-                                   <td> {{Carbon\Carbon::create($stdbook->from)->format(  'g:i A')}} -- {{Carbon\Carbon::create($stdbook->to)->format(' g:i A')}}</td>
-                                    <td>Rs {{$stdbook->total_amount}}</td>
-                                    <td>Rs {{$stdbook->discount}}</td>
-                                    <td>Rs {{$stdbook->advance}}</td>
-                                    <td>Rs {{$stdbook->rem_amount}}</td>
+                            <td>{{$stdbook['stadium']['name']}}</td>
+                            <td>{{$stdbook->stadium_type}}</td>
+                            <td>{{$stdbook->date}}</td>
+                            <td> {{Carbon\Carbon::create($stdbook->from)->format(  'g:i A')}} --
+                                {{Carbon\Carbon::create($stdbook->to)->format(' g:i A')}}</td>
+                            <td>Rs {{$stdbook->total_amount}}</td>
+                            <td>Rs {{$stdbook->discount}}</td>
+                            <td>Rs {{$stdbook->advance}}</td>
+                            <td>Rs {{$stdbook->rem_amount}}</td>
 
 
-                                    <td>
-                                    <span class="badge {{$stdbook->status=='Confirmed'?'bg-primary':'bg-danger'}}">  
+                            <td>
+                                <span class="badge {{$stdbook->status=='Confirmed'?'bg-primary':'bg-danger'}}">
                                     {{$stdbook->status}}
-</span>
-                                  </td>
-                                    <td>
-                                      <a href="/admin/stadium-bookings/{{$stdbook->id}}">
-                                      View <i style="width:17px;" data-feather="arrow-right"></i>
-                                      </a>
-                                    </td>
+                                </span>
+                            </td>
+                            <td>
+                                <a href="/admin/stadium-bookings/{{$stdbook->id}}">
+                                    View <i style="width:17px;" data-feather="arrow-right"></i>
+                                </a>
+                            </td>
 
-                                        
-                                </tr>
-                                @endforeach
+
+                        </tr>
+                        @endforeach
 
 
 
                     </tbody>
-                  </table>
-                </div>
-              </div>
+                </table>
             </div>
+        </div>
+    </div>
 </div>
 @endsection
