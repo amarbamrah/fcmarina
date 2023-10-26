2591f179b21394b76411275f6c82afe146828aa9
amarbamrah <jontybamrah@hotmail.com>
2023-10-27 00:28:07 +0530

    fd

--- app/Http/Controllers/api/StadiumController.php
+++ app/Http/Controllers/api/StadiumController.php
@@ -471,8 +471,8 @@ class StadiumController extends Controller
 
     public function applyDiscount(Request $request){
         $booking = StadiumBooking::find($request['booking_id']);
-        $booking->discount=$booking->discount+$request['amount'];
-        $booking->save();
+        // $booking->discount=$booking->discount+$request['amount'];
+        // $booking->save();
         return ['success'=>true];
     }
 
