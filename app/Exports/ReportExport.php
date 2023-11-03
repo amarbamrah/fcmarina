c803960ad1cefdbd27a0d9e7638b72f9c5cdafa5
amarbamrah <jontybamrah@hotmail.com>
2023-11-03 13:51:53 +0530

    bv

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -2,25 +2,17 @@
 
 namespace App\Exports;
 
-use App\Invoice;
-use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromView;
-
 use App\Models\Stadium;
 use App\Models\StadiumBooking;
 use Carbon\Carbon;
-
 use Carbon\CarbonPeriod;
-
-use Carbon\CarbonInterval;
-
-
-use App\Models\User;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
 
 class ReportExport implements FromView
 {
 
-    public function __construct($sid,$from,$to,$period)
+    public function __construct($sid, $from, $to, $period)
     {
         $this->period = $period;
         $this->sid = $sid;
@@ -29,79 +21,104 @@ class ReportExport implements FromView
 
     }
 
-
     public function view(): View
     {
 
-        $stds=Stadium::all();
+        $stds = Stadium::all();
 
         $stadiumbookings = StadiumBooking::query();
 
-        $month=$this->period=='curr'?Carbon::now():Carbon::now();
-
-
+        $month = $this->period == 'curr' ? Carbon::now() : Carbon::now();
 
         $dates = CarbonPeriod::since($month->startOfMonth())->days(1)->until($month->endOfMonth());
 
-        if($this->period=='custom'){
+        if ($this->period == 'custom') {
             $dates = CarbonPeriod::since(Carbon::create($this->from))->days(1)->until(Carbon::create($this->to));
-            
+
         }
         //return $month;
 
-
         //$dates=CarbonPeriod::create($month->startOfMonth(),$month->endOfMonth());
 
-        
+        $days = [];
 
+        $total_hours = 0;
+        $total_revs = 0;
 
-        $days=[];
+        $total_bookings = 0;
 
-        $total_hours=0;
-        $total_revs=0;
+        foreach ($dates as $date) {
 
-        $total_bookings=0;
+            $rev = 0;
+            $expRev = 0;
 
-        foreach($dates as $date){
+            $hours = 0;
 
-            
-            $rev=0;
-            $hours=0;
+            $pendRev = 0;
 
-            
-            if($this->sid!=null){
-                $bookings=StadiumBooking::where('stadium_id',$this->sid)->whereDate('date',$date)->get();
+            if ($request->has('stadium')) {
+                $bookings = StadiumBooking::where('stadium_id', $request['stadium'])->whereDate('date', $date)->where('status', '!=', 'Processing')->where('status', '!=', 'Cancelled')->where('booked_for', null)->get();
 
-            }else{
-                $bookings=StadiumBooking::whereDate('date',$date)->get();
+            } else {
+                $bookings = StadiumBooking::whereDate('date', $date)->where('status', '!=', 'Processing')->where('status', '!=', 'Cancelled')->where('booked_for', null)->get();
 
             }
 
-            $total_bookings+=count($bookings);
-            foreach($bookings as $booking){
-                if($booking->status!='Cancelled'){
-                $hours+=Carbon::create($booking->from)->floatDiffInHours(Carbon::create($booking->to));
-                $rev+=$booking->total_amount;
+            $total_bookings += count($bookings);
+            foreach ($bookings as $booking) {
+                if ($booking->status == 'Confirmed') {
+                    $rev += $booking->advance;
+                    $pendRev += $booking->rem_amount;
+                    $expRev += $booking->payable_amount;
+
                 }
+
+                if ($booking->status == 'Completed') {
+                    if (BookingPayment::where('booking_id', $booking->id)->where('payment_mode', 'Upi')->exists()) {
+                        $bp = BookingPayment::where('booking_id', $booking->id)->where('payment_mode', 'Upi')->first();
+                        $booking->upi = $bp->amount;
+
+                    } else {
+                        $booking->upi = 0;
+
+                    }
+
+                    if (BookingPayment::where('booking_id', $booking->id)->where('payment_mode', 'Cash')->exists()) {
+                        $bp = BookingPayment::where('booking_id', $booking->id)->where('payment_mode', 'Cash')->first();
+                        $booking->cash = $bp->amount;
+
+                    } else {
+                        $booking->cash = 0;
+
+                    }
+
+                    $rev += $booking->payable_amount;
+                    $expRev += $booking->payable_amount;
+
+                }
+
+                $hours += Carbon::create($booking->to)->floatDiffInHours(Carbon::create($booking->from));
             }
-            $day=[
-                'date'=>$date->format('D d M Y'),
-                'bookings'=>$bookings,
-                'rev'=>$rev,
-                'hours'=>$hours
+            $day = [
+                'date' => $date->format('D d M Y'),
+                'bookings' => $bookings,
+                'rev' => $rev,
+                'expRev' => $expRev,
+                'pendRev' => $pendRev,
+                'hours' => $hours,
             ];
 
-            $total_hours+=$hours;
-            $total_revs+=$rev;
-            
-            array_push($days,$day);
+            $total_hours += $hours;
+            $total_revs += $rev;
 
-            if($date->format('d-M-Y')==Carbon::now()->format('d-M-Y')){
+            array_push($days, $day);
+
+            if ($date->format('d-M-Y') == Carbon::now()->format('d-M-Y')) {
                 break;
             }
         }
 
-        return view('admin.reports.export',compact('stadiumbookings','stds','days','total_revs','total_bookings','total_hours'));
-        
+        return view('admin.reports.export', compact('stadiumbookings', 'stds', 'days', 'total_revs', 'total_bookings', 'total_hours'));
+
     }
 }
--- resources/views/admin/reports/export.blade.php
+++ resources/views/admin/reports/export.blade.php
@@ -1,49 +1,104 @@
 @foreach($days as $i=>$day)
 
-           
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Booking ID</th>
 
-                                    <th>User</th>
-                                    <th>User Phone</th>
-                                    <th>Stadium Name</th>
-                                    <th>Stadium Type</th>
-                                    <th>Booking Date</th>
-                                    <th>Booking Time</th>
-                                    <th>Total Amount</th>
-                                    <th>Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($day['bookings'] as $i=>$stdbook)
-                                <tr>
-                                    <td>{{$stdbook->booking_id}}</td>
-                                    <td>{{$stdbook['user']==null?$stdbook->name:$stdbook['user']['name']}}</td>
-                                    <td>{{$stdbook['user']==null?$stdbook->Phone:$stdbook['user']['phonenumber']}}</td>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Booking ID</th>
 
-                                    <td>{{$stdbook['stadium']['name']}}</td>
-                                    <td>{{$stdbook->stadium_type}}</td>
-                                    <td>{{$stdbook->date}}</td>
-                                    <td> {{Carbon\Carbon::create($stdbook->from)->format(  'g:i A')}} --
-                                        {{Carbon\Carbon::create($stdbook->to)->format(' g:i A')}}</td>
-                                    <td>Rs {{$stdbook->total_amount}}</td>
-                                    <td>
-                                        <span class="badge {{$stdbook->status=='Confirmed'?'bg-primary':'bg-danger'}}">
-                                            {{$stdbook->status}}
-                                        </span>
-                                    </td>
-                                   
-
-                                </tr>
-                                @endforeach
+            <th>User</th>
+            <th>User Phone</th>
+            <th>Stadium Name</th>
+            <th>Stadium Type</th>
+            <th>Booking Date</th>
+            <th>Booking Time</th>
+            <th>Booking Amount</th>
+            <th>Discount</th>
 
 
+            <th>Advance</th>
+            <th>Rem Amount</th>
 
-                            </tbody>
-                        </table>
+            <th>Total Amount</th>
+            <th>UPI</th>
+            <th>CASH</th>
 
-                
+            <th>Status</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($day['bookings'] as $i=>$stdbook)
+                                    <tr>
+                                        <td>{{$stdbook->booking_id}}</td>
+                                        <td>{{$stdbook['user']==null?$stdbook->name:$stdbook['user']['name']}}</td>
+                                        <td>{{$stdbook['user']==null?$stdbook->Phone:$stdbook['user']['phonenumber']}}
+                                        </td>
 
-            @endforeach
+                                        <td>{{$stdbook['stadium']['name']}}</td>
+                                        <td>{{$stdbook->stadium_type}}</td>
+                                        <td>{{$stdbook->date}}</td>
+                                        <td> {{Carbon\Carbon::create($stdbook->from)->format('g:i A')}} --
+                                            {{Carbon\Carbon::create($stdbook->to)->format('g:i A')}}</td>
+                                        <td>Rs {{$stdbook->total_amount}}</td>
+                                        <td>Rs {{$stdbook->discount}}</td>
+
+                                        
+                                        <td>Rs {{$stdbook->advance}}</td>
+
+                                        <td>Rs {{$stdbook->rem_amount}}</td>
+
+                                        <td>Rs {{$stdbook->payable_amount}}</td>
+
+                                        <td>
+                                        @if($stdbook->status=='Completed')
+                                        Rs {{$stdbook->upi}}
+                                        @endif
+                                       </td>
+
+                                        <td>
+                                        @if($stdbook->status=='Completed')
+                                        Rs {{$stdbook->cash}}
+                                        @endif
+                                      </td>
+
+                                        <td>
+                                            @if($stdbook->status=='Confirmed')
+                                            <span class="badge bg-primary">
+                                                {{$stdbook->status}}
+                                            </span>
+                                            @elseif($stdbook->status=='Completed')
+                                            <span class="badge bg-success">
+                                                {{$stdbook->status}}
+                                            </span>
+                                            @elseif($stdbook->status=='Cancelled')
+                                            <span class="badge bg-danger">
+                                                {{$stdbook->status}}
+                                            </span>
+
+                                            @elseif($stdbook->status=='Processing')
+                                            <span class="badge bg-info">
+                                                {{$stdbook->status}}
+                                            </span>
+
+                                            @else
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="/admin/stadium-bookings/{{$stdbook->id}}">
+                                                View <i style="width:17px;" data-feather="arrow-right"></i>
+                                            </a>
+                                        </td>
+
+
+                                    </tr>
+                                    @endforeach
+
+
+
+    </tbody>
+</table>
+
+
+
+@endforeach
