a780e84a2a0d6a04744e3f8175a53ac83ee02b59
amarbamrah <jontybamrah@hotmail.com>
2023-11-03 13:19:02 +0530

    ds

--- app/Http/Controllers/StadiumController.php
+++ app/Http/Controllers/StadiumController.php
@@ -208,10 +208,10 @@ class StadiumController extends Controller
 
 
             if ($request->has('stadium')) {
-                $bookings = StadiumBooking::where('stadium_id', $request['stadium'])->whereDate('date', $date)->get();
+                $bookings = StadiumBooking::where('stadium_id', $request['stadium'])->whereDate('date', $date)->where('status','!=','Processing')->where('status','!=','Cancelled')->get();
 
             } else {
-                $bookings = StadiumBooking::whereDate('date', $date)->get();
+                $bookings = StadiumBooking::whereDate('date', $date)->where('status','!=','Processing')->where('status','!=','Cancelled')->get();
 
             }
 
