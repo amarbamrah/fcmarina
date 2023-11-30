ad5301a07f027a10dab4c114a4ed51fc08892745
amarbamrah <jontybamrah@hotmail.com>
2023-11-30 16:07:25 +0530

    ds

--- resources/views/admin/stadiums/create.blade.php
+++ resources/views/admin/stadiums/create.blade.php
@@ -48,7 +48,7 @@
                                 <div class="mb-3">
                                     <label class="form-label">Stadium Type <span class="text-danger">*</span> </label>
                                     <select name="type" id="stypebox" class="form-select" required>
-                                        <option value="both">Both (5s+7s)</option>
+                                        <option value="both">5s+7s</option>
 
                                         <option value="5s">5s</option>
                                         <option value="7s">7s</option>
