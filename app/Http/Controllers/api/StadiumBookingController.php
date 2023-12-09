6241032069fbcc98f6da4f5d2ba3bb76dd81e553
amarbamrah <jontybamrah@hotmail.com>
2023-12-09 12:07:01 +0530

    ds

--- app/Http/Controllers/api/StadiumBookingController.php
+++ app/Http/Controllers/api/StadiumBookingController.php
@@ -287,14 +287,14 @@ class StadiumBookingController extends Controller
         if ($points > 1000) {
             $ptsToRedeem = 1000;
             $freeHours = $ptsToRedeem / 1000;
-            $pointMsg = 'Redeem ' . $ptsToRedeem . ' points to get ' . $freeHours . 'hr game free';
+            $pointMsg = 'Redeem ' . $ptsToRedeem . ' points to get 1000 Rs off';
         }
 
         $pointErrMsg = '';
 
         if ($redeem == 1 && $points > 1000) {
             $perHourPrice = $payableAmount / $hours;
-            $redeemDiscount = $freeHours * $perHourPrice;
+            $redeemDiscount = 1000;
 
             if ($redeemDiscount > $payableAmount) {
                 $redeemDiscount = $payableAmount;
@@ -744,14 +744,14 @@ class StadiumBookingController extends Controller
         if ($points > 1000) {
             $ptsToRedeem = 1000;
             $freeHours = $ptsToRedeem / 1000;
-            $pointMsg = 'Redeem ' . $ptsToRedeem . ' points to get ' . $freeHours . 'hr game free';
+            $pointMsg = 'Redeem ' . $ptsToRedeem . ' points to get 1000Rs off';
         }
 
         $pointErrMsg = '';
 
         if ($redeem == 1 && $points > 1000) {
             $perHourPrice = $payableAmount / $hours;
-            $redeemDiscount = $freeHours * $perHourPrice;
+            $redeemDiscount = 1000;
             if ($redeemDiscount > $payableAmount) {
                 $redeemDiscount = $payableAmount;
             }
