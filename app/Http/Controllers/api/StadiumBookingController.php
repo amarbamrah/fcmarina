707b1216379c071749af2e86e1ff5431a8b4b0f4
amarbamrah <jontybamrah@hotmail.com>
2024-02-15 11:26:16 +0530

    jk

--- app/Http/Controllers/api/StadiumBookingController.php
+++ app/Http/Controllers/api/StadiumBookingController.php
@@ -985,7 +985,12 @@ class StadiumBookingController extends Controller
             $api = new Api($key, $secret);
 
             // if ($user->phonenumber == '9311911065') {
-             $api->payment->fetch($request['payload']['payment']['entity']['id'])->capture(array('amount'=>$booking->advance*100,'currency' => 'INR'));
+                try{
+                    $api->payment->fetch($request['payload']['payment']['entity']['id'])->capture(array('amount'=>$booking->advance*100,'currency' => 'INR'));
+
+                }catch(Excpetion $e){
+                    
+                }
             // }
 
             $booking->status = 'Confirmed';
