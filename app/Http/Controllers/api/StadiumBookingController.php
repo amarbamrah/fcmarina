abb5ae065ed4a029227968284dc8972d007b0080
jonty <jontybamrah@hotmail.com>
2024-03-12 14:27:53 +0530

    fd

--- app/Http/Controllers/api/StadiumBookingController.php
+++ app/Http/Controllers/api/StadiumBookingController.php
@@ -901,7 +901,7 @@ class StadiumBookingController extends Controller
 
         $payableAmount = $request['total_amount'];
 
-        $bCount = StadiumBooking::where('user_id', $request['user_id'])->count();
+        $bCount = StadiumBooking::where('user_id', $request['user_id'])->where('status','!=','Processing')->count();
 
         $points = $user->points;
 
